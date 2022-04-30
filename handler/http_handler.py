4a2af08a4989a968f0245e62b143ba04ba0c507c
jason85115 <jason85115@gmail.com>
2022-04-30 12:17:04 +0800

    修正 handler

--- handler/http_handler.py
+++ handler/http_handler.py
@@ -1,14 +1,14 @@
-from fastapi import Request
+from fastapi import Request, status
 from fastapi.exceptions import RequestValidationError
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
 
 # define or import your error response model
 class HttpValidationErrorResponse(BaseModel):
-    message: str = Field(..., example="message to the developer")
+    message: str = Field(..., example="Occur a Validation Error. Please Checking Field Name or Field Type is correct.")
     error: str = Field(..., example="Validation Error")
-    detail: dict = Field(..., example=[
-        {'loc': 'quantity', 
+    detail: list = Field(..., example=[
+        {'loc': '(body, field name)', 
          'msg': 'value is not a valid integer',
          'type':'type_error.integer'}])
 
@@ -20,6 +20,9 @@ def init_http_exception_handler(app):
 
     @app.exception_handler(RequestValidationError)
     async def validation_exception_handler(request: Request, err: RequestValidationError):
-        print(err.errors())
-        print(err.body)
-        return PlainTextResponse(str(err), status_code=422)
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            content= HttpValidationErrorResponse(message= "Occur a Validation Error. Please Checking Field Name or Field Type is correct.", 
+                                                 error= "Validation Error", 
+                                                 detail= err.errors()).json()
+        )
